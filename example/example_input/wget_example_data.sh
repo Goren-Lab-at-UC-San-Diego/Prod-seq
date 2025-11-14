ec90b053b465abce564c53ba15b30c6f22f293bd
poloxu <xtypolo@gmail.com>
2025-11-13 22:27:18 -0800

    cleanup data download

--- example/example_input/wget_example_data.sh
+++ example/example_input/wget_example_data.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-wget -i example_data_links.txt 
+wget http://homer.ucsd.edu/tix034/prod_example/PQ_HEK_DMSO1_R1.fastq.gz
+
+wget http://homer.ucsd.edu/tix034/prod_example/PQ_HEK_DMSO2_R1.fastq.gz
+
+wget http://homer.ucsd.edu/tix034/prod_example/PQ_HEK_Taz1_R1.fastq.gz
+
+wget http://homer.ucsd.edu/tix034/prod_example/PQ_HEK_Taz2_R1.fastq.gz
+
+wget http://homer.ucsd.edu/tix034/prod_example/Prod_HEK_DMSO1_R1.fastq.gz
+
+wget http://homer.ucsd.edu/tix034/prod_example/Prod_HEK_DMSO1_R2.fastq.gz
+
+wget http://homer.ucsd.edu/tix034/prod_example/Prod_HEK_DMSO2_R1.fastq.gz
+
+wget http://homer.ucsd.edu/tix034/prod_example/Prod_HEK_DMSO2_R2.fastq.gz
+
+wget http://homer.ucsd.edu/tix034/prod_example/Prod_HEK_Taz1_R1.fastq.gz
+
+wget http://homer.ucsd.edu/tix034/prod_example/Prod_HEK_Taz1_R2.fastq.gz
+
+wget http://homer.ucsd.edu/tix034/prod_example/Prod_HEK_Taz2_R1.fastq.gz
+
+wget http://homer.ucsd.edu/tix034/prod_example/Prod_HEK_Taz2_R2.fastq.gz
